165eb5a2ccbe2d8b551c49a61058de41827b1567
Oleksii <achili3f17@gmail.com>
2023-03-21 19:22:26 +0200

    readme

--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -65,9 +65,9 @@ export const Circle = styled.div`
   border-radius: 50%;
   overflow: hidden;
   border: 8px solid #ebd8ff;
-  /* box-shadow: 0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06),
+  box-shadow: 0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06),
     inset 0px -2.19582px 4.39163px #ae7be3,
-    inset 0px 4.39163px 3.29372px #fbf8ff; */
+    inset 0px 4.39163px 3.29372px #fbf8ff;
   background: linear-gradient(
     114.99deg,
     #471ca9 -0.99%,
